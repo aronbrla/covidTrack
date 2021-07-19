9194cfa087b85f07732632c2282fef371856e647
Luis AB <labr.015445@gmail.com>
2021-07-18 21:59:18 -0500

    Dash Paciente: Barra lateral y conexión hecha

--- routes/routes.js
+++ routes/routes.js
@@ -18,7 +18,26 @@ router.get('/dash',(peticion,respuesta)=>{
     respuesta.render('dash');
 });
 
+//Rutas del dash Paciente
 router.get('/paciente',(peticion,respuesta)=>{
     respuesta.render('../views/paciente/index.ejs');
 });
+
+router.get('/paciente/doctor',(peticion,respuesta)=>{
+    respuesta.render('../views/paciente/sites/doctor.ejs');
+});
+
+router.get('/paciente/citas',(peticion,respuesta)=>{
+    respuesta.render('../views/paciente/sites/citas.ejs');
+});
+
+router.get('/paciente/ajustes',(peticion,respuesta)=>{
+    respuesta.render('../views/paciente/sites/ajustes.ejs');
+});
+
+router.get('/paciente/formulario',(peticion,respuesta)=>{
+    respuesta.render('../views/paciente/sites/formulario.ejs');
+});
+//Fin de las rutas del dash paciente
+
 module.exports = router;
