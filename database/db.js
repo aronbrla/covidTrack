4c1a0692c5d306d4e0c9b74fe4704335202dcd93
Luis AB <labr.015445@gmail.com>
2021-08-02 20:58:43 -0500

    cambios de db

--- database/db.js
+++ database/db.js
@@ -1,15 +1,15 @@
 const mysql =require('mysql');
 const connection =mysql.createConnection({
-    host: "us-cdbr-east-04.cleardb.com" || "localhost",
-    database: "heroku_c960b61942b19d8" || "covidtrack",
-    user: 'b9bce1223109e1' || "root",
-    password: '141d05d2' || "",
+    host: process.env.HOST || "localhost",
+    database: process.env.DATABASE || "covidtrack",
+    user: process.env.USER || "root",
+    password: process.env.PASSWORD || "",
     multipleStatements: true
 });
 connection.connect((error)=>{
     if(error){
-    console.log('Error: '+error);
-    return;
+        console.log('Error: '+error);
+        return;
     } 
     console.log('CONECTADO A LA BASE DE DATOS!');
 });
