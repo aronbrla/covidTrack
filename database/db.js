5ab8a492f5511faae4c45e79a545f99181f775b3
Luis AB <labr.015445@gmail.com>
2021-08-02 19:00:27 -0500

    codigo sql de base de datos en app.txt

--- database/db.js
+++ database/db.js
@@ -1,9 +1,9 @@
 const mysql =require('mysql');
 const connection =mysql.createConnection({
-    host: "localhost",
-    database: "covidtrack",
-    user: "root",
-    password: "",
+    host: process.env.HOST || "localhost",
+    database: process.env.DATABASE || "covidtrack",
+    user: process.env.USER || "root",
+    password: process.env.PASSWORD || "",
     multipleStatements: true
 });
 connection.connect((error)=>{
