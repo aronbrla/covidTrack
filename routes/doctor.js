aeaea17ebf1eaa497c9b818f8f371a25157805de
Luis Aaron Blas <92341949+blasluis@users.noreply.github.com>
2022-04-11 12:41:37 -0500

    Corrigiendo algunos condiciones de entrada de datos

--- routes/doctor.js
+++ routes/doctor.js
@@ -9,9 +9,9 @@ router.use((req, res, next) => {                        // cambiando layout para
   next();
 });
 
-const {getPacByDocDNI, getFormsByDocDNI, getCitasDetailsByDocDNI, 
-      updateDocByDNI, getDocByDNI, updateDocPassByMail, 
-      createCita, deleteCita} = require('../database/queries')
+const { getUserByMail, getPacByDocDNI, getFormsByDocDNI, getCitasDetailsByDocDNI,
+  updateDocByDNI, getDocByDNI, updateDocPassByMail,
+  createCita, deleteCita } = require('../database/queries')
 
 //Rutas del dash DOCTOR
 router.get("/", (req, res) => {
@@ -39,26 +39,26 @@ router.get("/informacion", (req, res) => {
 
 router.get("/pacientes", (req, res) => {
   connection.query(getPacByDocDNI, [req.session.DNIDOCTOR], async (err, results) => {
-      if (err) {
-        console.log("ERROR: " + err);
-      } else {
-        connection.query(getFormsByDocDNI, [req.session.DNIDOCTOR], async (err, results1) => {
-            if (err) {
-              console.log("ERROR: " + err);
-            } else {
-              console.log("pruf");
-              console.log(results);
-              console.log(results1);
-              res.render("doctor/pacientes", {
-                title: 'Pacientes',
-                listapacientes: JSON.stringify(results),
-                listaFormularios: JSON.stringify(results1),
-              });
-            }
-          }
-        );
+    if (err) {
+      console.log("ERROR: " + err);
+    } else {
+      connection.query(getFormsByDocDNI, [req.session.DNIDOCTOR], async (err, results1) => {
+        if (err) {
+          console.log("ERROR: " + err);
+        } else {
+          console.log("pruf");
+          console.log(results);
+          console.log(results1);
+          res.render("doctor/pacientes", {
+            title: 'Pacientes',
+            listapacientes: JSON.stringify(results),
+            listaFormularios: JSON.stringify(results1),
+          });
+        }
       }
+      );
     }
+  }
   );
 });
 
@@ -67,43 +67,43 @@ router.get("/citas", (req, res) => {
   let pacienteList = []
   citasList = [{}]
   const citasSQL = getCitasDetailsByDocDNI
-  connection.query(citasSQL,[req.session.DNIDOCTOR], async (error, results) => {
-      // console.log(results)
-      results.map(cita => {
-        citasList.push({
-          pacDNI: cita.pac_dni,
-          todo: `${cita.pac_apellidos} ${cita.pac_nombres}`,
-          date: cita.fecha,
+  connection.query(citasSQL, [req.session.DNIDOCTOR], async (error, results) => {
+    // console.log(results)
+    results.map(cita => {
+      citasList.push({
+        pacDNI: cita.pac_dni,
+        todo: `${cita.pac_apellidos} ${cita.pac_nombres}`,
+        date: cita.fecha,
+      });
+    })
+    connection.query(getPacByDocDNI, [req.session.DNIDOCTOR], async (error, results) => {
+      for (let i = 0; i < results.length; i++) {
+        pacienteList.push({
+          dni: results[i].pac_dni,
+          nombre: results[i].pac_apellidos + " " + results[i].pac_nombres,
         });
-      })
-      connection.query(getPacByDocDNI, [req.session.DNIDOCTOR], async (error, results) => {
-          for (let i = 0; i < results.length; i++) {
-            pacienteList.push({
-              dni: results[i].pac_dni,
-              nombre: results[i].pac_apellidos + " " + results[i].pac_nombres,
-            });
-          }
-          res.render("doctor/citas", {
-            title: 'Citas',
-            dnid: req.session.DNIDOCTOR,
-            pacientes: JSON.stringify(pacienteList),
-            doc: req.session.NOMBREDOCTOR,
-            citasList: JSON.stringify(citasList),
-          });
-        }
-      );
+      }
+      res.render("doctor/citas", {
+        title: 'Citas',
+        dnid: req.session.DNIDOCTOR,
+        pacientes: JSON.stringify(pacienteList),
+        doc: req.session.NOMBREDOCTOR,
+        citasList: JSON.stringify(citasList),
+      });
     }
+    );
+  }
   );
 });
 
 router.get("/chat", (req, res) => {
   connection.query(getPacByDocDNI, [req.session.DNIDOCTOR], async (error, results) => {
-      res.render("doctor/chat", {
-        title: 'Chat',
-        dnioculto: req.session.DNIDOCTOR,
-        listapacientes: JSON.stringify(results),
-      });
-    }
+    res.render("doctor/chat", {
+      title: 'Chat',
+      dnioculto: req.session.DNIDOCTOR,
+      listapacientes: JSON.stringify(results),
+    });
+  }
   );
 });
 
@@ -142,27 +142,27 @@ router.post("/editar", async (req, res) => {
   const celular = req.body.phone;
   const email = req.body.email;
   connection.query(updateDocByDNI, [email, celular, req.session.DNIDOCTOR], async (error, results) => {
-      if (error) {
-        console.log(error);
-      } else {
-        connection.query(getDocByDNI, [req.session.DNIDOCTOR],
-          async (error, results) => {
-            if (error) {
-              console.log(error);
-            } else {
-              req.session.loggedin = true;
-              req.session.NOMBREDOCTOR = results[0].doc_nombres + " " + results[0].doc_apellidos;
-              req.session.CORREODOCTOR = results[0].doc_email;
-              req.session.DNIDOCTOR = results[0].doc_dni;
-              req.session.TELEFONODOCTOR = results[0].doc_celular;
-              req.session.SEXODOC = results[0].doc_sexo;
-              
-              res.redirect('/doctor/ajustes')
-            }
+    if (error) {
+      console.log(error);
+    } else {
+      connection.query(getDocByDNI, [req.session.DNIDOCTOR],
+        async (error, results) => {
+          if (error) {
+            console.log(error);
+          } else {
+            req.session.loggedin = true;
+            req.session.NOMBREDOCTOR = results[0].doc_nombres + " " + results[0].doc_apellidos;
+            req.session.CORREODOCTOR = results[0].doc_email;
+            req.session.DNIDOCTOR = results[0].doc_dni;
+            req.session.TELEFONODOCTOR = results[0].doc_celular;
+            req.session.SEXODOC = results[0].doc_sexo;
+
+            res.redirect('/doctor/ajustes')
           }
-        );
-      }
+        }
+      );
     }
+  }
   );
 });
 
@@ -171,7 +171,7 @@ router.post("/EditCon", async (req, res) => {
   const npass = req.body.passwordNew1;
   const cpass = req.body.passwordNew2;
   if (pass && npass && cpass) {
-    connection.query(getUserByMail, [req.session.CORREODOCTOR],
+    connection.query(getUserByMail, ["doctor", req.session.CORREODOCTOR],
       async (error, results) => {
         if (pass != results[0][0].doc_contrasenia) {
           res.send("La contraseña actual es incorrecta.");
@@ -227,16 +227,16 @@ router.post("/addCita", async (req, res) => {
 });
 
 router.post("/deleteCita", async (req, res) => {
-  const {pacienteDni, dateTime} = req.body
+  const { pacienteDni, dateTime } = req.body
   const delCitaSQL = deleteCita
   // console.log(evento);
-  connection.query(delCitaSQL,[pacienteDni, dateTime], async (error, results) => {
-      if (error) {
-        console.error(error);
-      } else {
-        res.redirect("/doctor/citas");
-      }
+  connection.query(delCitaSQL, [pacienteDni, dateTime], async (error, results) => {
+    if (error) {
+      console.error(error);
+    } else {
+      res.redirect("/doctor/citas");
     }
+  }
   );
 });
 
--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const bcryptjs = require("bcryptjs");         // invocamos a bcryptjs
 const pacienteRouter = require("./paciente");
 const doctorRouter = require("./doctor");
 
-const {getUserByMail, getDocByDNI, getPacsDNI, getCitasByDocDNI, getPacByDNI, createPac} = require('../database/queries')
+const { getUserByMail, getDocByDNI, getPacsDNI, getCitasByDocDNI, getPacByDNI, createPac } = require('../database/queries')
 
 router.get("/", (peticion, respuesta) => {
   respuesta.render("index");
@@ -21,57 +21,58 @@ router.get("/register", (peticion, respuesta) => {
 // Creación de Paciente Ruta
 router.post("/register", async (req, res) => {
   //capturando datos
-  const {mail, name, lastname, 
-        pass, dni, address, phone, 
-        date, distrito, sexo} = req.body
+  const { mail, name, lastname,
+    pass, dni, address, phone,
+    date, distrito, sexo } = req.body
+  const phoneString = phone.toString()
   //encriptando la contraseña
   let passwordHaas = await bcryptjs.hash(pass, 8);
   //buscamos correo y dni, si ya están en la base de datos no podrá registrarse:
   // console.log(req.body);
   connection.query(getUserByMail, ["paciente", mail], async (error, results) => {
-      if (error) {
-        console.log(error);
+    if (error) {
+      console.log(error);
+    } else {
+      if (results[0].length != 0) {
+        res.send("USUARIO YA REGISTRADO");
       } else {
-        if (results[0].length != 0) {
-          res.send("USUARIO YA REGISTRADO");
-        } else {
-          connection.query(getPacByDNI,[dni],async (error, results) => {
-              if (error) {
-                console.log(error);
-              } else {
-                if (results.length != 0) {
-                  res.send("USUARIO YA REGISTRADO");
-                } else {
-                  //si no encuentra los datos, se puede registrar
-                  connection.query(createPac,
-                    {
-                      pac_nacimiento: date,
-                      pac_dni: dni,
-                      pac_apellidos: lastname,
-                      pac_nombres: name,
-                      pac_email: mail,
-                      pac_contrasenia: passwordHaas,
-                      pac_celular: phone,
-                      pac_direccion: address,
-                      pac_distrito: distrito,
-                      pac_sexo: sexo,
-                      doc_dni: "72865690",
-                    },
-                    async (error, results) => {
-                      if (error) {
-                        console.log(error);
-                      } else {
-                        res.redirect('/login');
-                      }
-                    }
-                  );
+        connection.query(getPacByDNI, [dni], async (error, results) => {
+          if (error) {
+            console.log(error);
+          } else {
+            if (results.length != 0) {
+              res.send("USUARIO YA REGISTRADO");
+            } else {
+              //si no encuentra los datos, se puede registrar
+              connection.query(createPac,
+                {
+                  pac_nacimiento: date,
+                  pac_dni: dni,
+                  pac_apellidos: lastname,
+                  pac_nombres: name,
+                  pac_email: mail,
+                  pac_contrasenia: passwordHaas,
+                  pac_celular: phoneString,
+                  pac_direccion: address,
+                  pac_distrito: distrito,
+                  pac_sexo: sexo,
+                  doc_dni: "72865690",
+                },
+                async (error, results) => {
+                  if (error) {
+                    console.log(error);
+                  } else {
+                    res.redirect('/login');
+                  }
                 }
-              }
+              );
             }
-          );
+          }
         }
+        );
       }
     }
+  }
   );
 });
 
@@ -83,97 +84,97 @@ router.post("/auth", async (req, res) => {
     res.end()
   }
   if (usertype == "paciente") {
-    connection.query(getUserByMail,[usertype, usermail], async (err, results) => {
-        if (err) return console.error(err);
-        // console.log(results)
-        const user = {...results[0][0]}
-        if (results[0].length == 0 || !(await bcryptjs.compare(pass, user.pac_contrasenia))) {
-          res.send("Email o contraseña incorrecta")
-          res.end()
-        }
-        req.session.loggedin = true;
-        req.session.NOMBRe = `${user.pac_nombres} ${user.pac_apellidos}`;
-        req.session.CORREo = user.pac_email;
-        req.session.DIRECCIOn = user.pac_direccion;
-        req.session.DNi = user.pac_dni;
-        req.session.TELEFONo = user.pac_celular;
-        req.session.SEXo = user.pac_sexo;
-        req.session.DISTRITo = user.pac_distrito;
-        req.session.DNIDOCTOR1 = user.doc_dni;
-        let fecha = user.pac_nacimiento;
-        let a = fecha.toString();
-        let b = a.substring(4, 15);
-        let citasList = [{}];
-        req.session.EDAd = b;
-        // console.log(req.session.NOMBRe);
-        connection.query(getDocByDNI,["72865690"], async (error, results) => {
-            if (error) {
-              console.error(error);
-            } else {
-              req.session.NOMDOC = `${results[0].doc_nombres} ${results[0].doc_apellidos}`;
-              req.session.CORDOC = results[0].doc_email;
-              req.session.COLDOC = results[0].doc_especialidad;
-              req.session.SEXODOC = results[0].doc_sexo;
-              
-              res.redirect('/paciente')
-            }
-          }
-        );
+    connection.query(getUserByMail, [usertype, usermail], async (err, results) => {
+      if (err) return console.error(err);
+      // console.log(results)
+      const user = { ...results[0][0] }
+      if (results[0].length == 0 || !(await bcryptjs.compare(pass, user.pac_contrasenia))) {
+        res.send("Email o contraseña incorrecta")
+        res.end()
       }
+      req.session.loggedin = true;
+      req.session.NOMBRe = `${user.pac_nombres} ${user.pac_apellidos}`;
+      req.session.CORREo = user.pac_email;
+      req.session.DIRECCIOn = user.pac_direccion;
+      req.session.DNi = user.pac_dni;
+      req.session.TELEFONo = user.pac_celular;
+      req.session.SEXo = user.pac_sexo;
+      req.session.DISTRITo = user.pac_distrito;
+      req.session.DNIDOCTOR1 = user.doc_dni;
+      let fecha = user.pac_nacimiento;
+      let a = fecha.toString();
+      let b = a.substring(4, 15);
+      let citasList = [{}];
+      req.session.EDAd = b;
+      // console.log(req.session.NOMBRe);
+      connection.query(getDocByDNI, ["72865690"], async (error, results) => {
+        if (error) {
+          console.error(error);
+        } else {
+          req.session.NOMDOC = `${results[0].doc_nombres} ${results[0].doc_apellidos}`;
+          req.session.CORDOC = results[0].doc_email;
+          req.session.COLDOC = results[0].doc_especialidad;
+          req.session.SEXODOC = results[0].doc_sexo;
+
+          res.redirect('/paciente')
+        }
+      }
+      );
+    }
     );
   } else {
-      connection.query(getUserByMail,[usertype, usermail], async (err, results, fields) => {
-        if (err) throw err
-        // console.log(results)
-        const user = {...results[0][0]}
-        if (results.length == 0 || pass != user.doc_contrasenia) {
-          res.send("Email o contraseña incorrecta")
-          res.end()
-        }
-        req.session.loggedin = true;
-        req.session.NOMBREDOCTOR = `${user.doc_nombres} ${user.doc_apellidos}`;
-        req.session.CORREODOCTOR = user.doc_email;
-        req.session.TELEFONODOCTOR = user.doc_celular;
-        req.session.DNIDOCTOR = user.doc_dni;
+    connection.query(getUserByMail, [usertype, usermail], async (err, results, fields) => {
+      if (err) throw err
+      // console.log(results)
+      const user = { ...results[0][0] }
+      if (results.length == 0 || pass != user.doc_contrasenia) {
+        res.send("Email o contraseña incorrecta")
+        res.end()
+      }
+      req.session.loggedin = true;
+      req.session.NOMBREDOCTOR = `${user.doc_nombres} ${user.doc_apellidos}`;
+      req.session.CORREODOCTOR = user.doc_email;
+      req.session.TELEFONODOCTOR = user.doc_celular;
+      req.session.DNIDOCTOR = user.doc_dni;
 
-        connection.query(getPacsDNI,async (error, results) => {
-            req.session.NUMEROPACIENTES = results.length;
+      connection.query(getPacsDNI, async (error, results) => {
+        req.session.NUMEROPACIENTES = results.length;
 
-            var hoy = new Date(
-              new Date(new Date(new Date()).toISOString()).getTime() -
+        var hoy = new Date(
+          new Date(new Date(new Date()).toISOString()).getTime() -
+          new Date().getTimezoneOffset() * 60000
+        )
+          .toISOString()
+          .slice(0, 19)
+          .replace("T", " ");
+
+        connection.query(getCitasByDocDNI, [req.session.DNIDOCTOR], async (error, results) => {
+          console.log(results)
+          const today = new Date().toISOString()
+          console.log(today)
+          for (let i = 0; i < results.length; i++) {
+            var bbdd = new Date(
+              new Date(
+                new Date(new Date(results[i].fecha)).toISOString()
+              ).getTime() -
               new Date().getTimezoneOffset() * 60000
             )
               .toISOString()
               .slice(0, 19)
               .replace("T", " ");
-
-            connection.query(getCitasByDocDNI,[req.session.DNIDOCTOR], async (error, results) => {
-              console.log(results)
-              const today = new Date().toISOString()
-              console.log(today)
-                for (let i = 0; i < results.length; i++) {
-                  var bbdd = new Date(
-                    new Date(
-                      new Date(new Date(results[i].fecha)).toISOString()
-                    ).getTime() -
-                    new Date().getTimezoneOffset() * 60000
-                  )
-                    .toISOString()
-                    .slice(0, 19)
-                    .replace("T", " ");
-                  // console.log("BBDD: " + bbdd + " HOY: " + hoy);
-                  if (hoy == bbdd) {
-                    // console.log("fechas iguales");
-                    // console.log("BBDD: " + bbdd + " HOY: " + hoy);
-                  }
-                }
-                req.session.NUMEROCITAS = results.length;
-                res.redirect('/doctor')
-              }
-            );
+            // console.log("BBDD: " + bbdd + " HOY: " + hoy);
+            if (hoy == bbdd) {
+              // console.log("fechas iguales");
+              // console.log("BBDD: " + bbdd + " HOY: " + hoy);
+            }
           }
+          req.session.NUMEROCITAS = results.length;
+          res.redirect('/doctor')
+        }
         );
       }
+      );
+    }
     );
   }
 });
